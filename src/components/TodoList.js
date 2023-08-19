339dd3b428a0059ceb6d3c72a2f8cc67c2a44a4b
Shaikh Haris Ali <57299748+ShaikhHarisAli@users.noreply.github.com>
2023-08-19 13:41:12 +0500

    Initial commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const TodoList = ({todos,setTodos,setEditTodo}) => {
+
+    const handleComplete = (todo) =>{
+        setTodos(todos.map((item)=>{
+            if(item.id === todo.id){
+                return {...item,completed: !item.completed}
+            }
+            return item;
+        }))
+    }
+
+    const handleDelete =({id})=>{
+        setTodos(todos.filter((todo)=> todo.id !== id))
+    }
+
+    const handleEdit =({id})=>{
+        const findTodo = todos.find((todo)=>{ return todo.id === id})
+        setEditTodo(findTodo)
+    }
+
+
+  return (
+    <div>
+      {
+        todos.map((todo)=>{
+           return <li className='list-item' key={todo.id}>
+                <input type='text' className={`list ${todo.completed ? "complete": ""}`} value={todo.title} onChange={(event)=>event.preventDefault()}/>
+                <div>
+                    <button className='button-complete task-button' onClick={()=> handleComplete(todo)} >
+                        <i className='fa fa-check-circle'></i>
+                    </button>
+                    <button className='button-edit task-button' onClick={()=> handleEdit(todo)}>
+                        <i className='fa fa-edit'></i>
+                    </button>
+                    <button className='button-delete task-button' onClick={()=> handleDelete(todo)} >
+                        <i className='fa fa-trash'></i>
+                    </button>
+                </div>
+            </li>
+        })
+      }
+    </div>
+  )
+}
+
+export default TodoList
